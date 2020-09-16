cc1aac0f6651bf09f55c03dcf675dd5080ac2181
wgm425 <wgm425@gmail.com>
2020-09-16 04:18:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int [] a = {5,4,3,2,1};
+
+    
+    for (int i = 0; i < a.length; i ++)
+      System.out.println("a["+i+"] = "+a[i]);
+
+  }
+}
